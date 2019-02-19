3b0671c679b0e13e45af2aaf216727d9fe3e6689
Alwin Rivera <aimrivera@gmail.com>
2019-02-20 00:27:22 +0800

    Add files via upload

--- shrink.js
+++ shrink.js
@@ -2,10 +2,10 @@
 
         function scrollFunction() {
             if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                document.getElementById("navbar-brand").setAttribute('style', "font-size:20px; margin-top:4px; margin-bottom:4px; width:68.281px; height:24px;");
-                document.getElementById("navbar-brand").style.transition = "all .5s";
+                document.getElementById("navigation").setAttribute('style', "background-color: #2c86c7!important;");
+                document.getElementById("navigation").style.transition = "all .5s";
             } else {
-                document.getElementById("navbar-brand").setAttribute('style', "font-size:24px; margin-bottom:0!important; width:68.281px;");
-                document.getElementById("navbar-brand").style.transition = "all .5s";
+                document.getElementById("navigation").setAttribute('style', "background-color: #fff!important;");
+                document.getElementById("navigation").style.transition = "all .5s";
             }
         }
